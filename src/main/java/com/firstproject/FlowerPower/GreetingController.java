c57d4e2a8d5549f635cea807a3c319f2dbaaae47
termyn9 <hrya4kov.ilya@yandex.ru>
2020-03-09 12:21:36 +0300

    shop

--- src/main/java/com/firstproject/FlowerPower/GreetingController.java
+++ src/main/java/com/firstproject/FlowerPower/GreetingController.java
@@ -3,41 +3,50 @@ package com.firstproject.FlowerPower;
 import com.firstproject.FlowerPower.domain.Message;
 import com.firstproject.FlowerPower.repository.MessageRep;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
-@Controller
+@RestController
 public class GreetingController { //слушает запросы от пользователя по map
-    @Autowired MessageRep messageRep;
-    @GetMapping("/greeting")      //и возвращает данные
+    @Autowired
+    MessageRep messageRep;
+
+    @RequestMapping("/")      //и возвращает данные
     public String greeting(
             //Полное описание RequestParam c написанеим от руки ()
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
+            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
             Map<String, Object> model //то, куда складыать данные, вовзращаемые пользователю
     ) { //Map-стр-ра java с ключем string, значением object
         model.put("name", name);
         return "greeting"; //возвращаемый файл-шаблон, описываемый далее
     }
-        @GetMapping
-        public String main(Map<String, Object> model){
+
+    @GetMapping
+    public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRep.findAll();
         model.put("messages", messages);
-            return "main";
-        }
+        return "main";
+    }
 
-        @PostMapping //RequestParam дёргает значение из описанных полей (краткое)
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
+    @PostMapping //RequestParam дёргает значение из описанных полей (краткое)
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
         Message message = new Message(text, tag); //сохранение
         messageRep.save(message);
 
-            Iterable<Message> messages = messageRep.findAll(); //берем из репо
-            model.put("messages", messages); //кладём в модель
+        Iterable<Message> messages = messageRep.findAll(); //берем из репо
+        model.put("messages", messages); //кладём в модель
 
         return "main"; //отдали пользователю
-        }
+    }
 
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        List<Message> messages = messageRep.findByTag(filter);
+
+        model.put("messages", messages);
+
+        return "main";
+    }
 }
--- src/main/java/com/firstproject/FlowerPower/repository/MessageRep.java
+++ src/main/java/com/firstproject/FlowerPower/repository/MessageRep.java
@@ -3,6 +3,9 @@ package com.firstproject.FlowerPower.repository;
 import com.firstproject.FlowerPower.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 //получение списка полей или по идентификатору
 public interface MessageRep extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
 }
